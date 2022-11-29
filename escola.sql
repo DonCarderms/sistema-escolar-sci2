7f2fc4f1b5abb6080b05d995b04e8edeb1e7cb81
Don Carderms <donphilocardermssouffrant@gmail.com>
2022-11-29 14:52:58 -0300

    add input de mudar de cursos e turmas no editar aluno

--- escola.sql
+++ escola.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: projetos
--- Tempo de geração: 17/11/2022 às 00:37
+-- Tempo de geração: 29/11/2022 às 17:51
 -- Versão do servidor: 8.0.30
 -- Versão do PHP: 8.0.24
 
@@ -39,11 +39,18 @@ CREATE TABLE `curso` (
 --
 
 INSERT INTO `curso` (`id`, `nome`, `dataInicio`, `dataFinal`) VALUES
-(1, 'Programação Web em php ', '2022-11-30', '2023-07-12'),
-(2, 'Marketing', '2023-01-12', '2023-02-16'),
-(3, 'atendimento', '2022-11-11', NULL),
-(4, 'Exel Basico ao advançado', '2022-11-24', '2023-01-27'),
-(6, 'Desenvolvimento Web Completo 2022 - 20 cursos + 20 projetos', '2022-11-22', NULL);
+(1, 'Programação Web em php', '2023-01-16', '2023-09-12'),
+(2, 'Marketing ', '2023-01-12', '2023-02-16'),
+(3, 'Atendimento  ', '2022-11-11', '2023-03-17'),
+(6, 'Desenvolvimento Web Completo 2022 - 20 cursos + 20 projetos', '2022-11-22', NULL),
+(7, 'Curso de JavaScript Completo do iniciante ao mestre ', '2022-11-19', NULL),
+(10, 'Contabilidade e Finanças', '2022-11-26', NULL),
+(28, 'Gestão de Recursos Humanos', '2022-11-26', '2023-01-27'),
+(29, 'Aprender React native ', '2023-02-10', NULL),
+(32, 'Infraestrutura de Ti', '2023-01-28', '2023-05-26'),
+(39, 'curso padrão', '2022-11-17', NULL),
+(41, 'Data science Big data master', '2023-03-24', NULL),
+(45, 'Engenharia e Arquitetura  ', '2023-02-23', '2023-06-20');
 
 -- --------------------------------------------------------
 
@@ -62,8 +69,22 @@ CREATE TABLE `endereco` (
 --
 
 INSERT INTO `endereco` (`id`, `logradouro`, `numero`) VALUES
-(1, 'Arthur pereguda', 800),
-(3, 'Alameda Rio Branco', 378);
+(1, 'Arthur peguda  ', 800),
+(3, 'Alameda Rio Branco', 378),
+(29, 'rodolf setrem', 40),
+(61, 'Arthur peguda ', 56),
+(69, '2ª Avenida Mandchúria', 640),
+(87, 'Travessa Almirante Ary Parreiras', 665),
+(89, 'Beco Paribaroba', 140),
+(90, 'Rua Caribe', 548),
+(92, 'Arthur peguda ', 56),
+(93, 'Arthur peguda ', 905),
+(94, 'Arthur peguda ', 905),
+(95, 'Arthur peguda ', 32),
+(96, 'Arthur peguda g', 32),
+(97, 'Arthur peguda g', 32),
+(98, 'Arthur peguda g', 32),
+(99, 'Arthur peguda ', 32);
 
 -- --------------------------------------------------------
 
@@ -126,8 +147,15 @@ CREATE TABLE `turma` (
 --
 
 INSERT INTO `turma` (`id`, `nome`, `codigo`, `curso_id`) VALUES
-(1, 'programação web', 'MDKKVDFK', 1),
-(2, 'Programação Web ', 'MENRJ45M4', 1);
+(1, 'Progrmação php', 'MDKKVDFK', 1),
+(2, 'Atendimento', 'MENRJ45M4', 3),
+(3, 'turma padrão', 'R4747NFD', 39),
+(25, 'Super da ti', 'DFNGMDFNGM4444', 32),
+(26, 'big maters data', 'BSDHFJSDHF6575', 41),
+(47, 'react', 'HSKJ3729', 29),
+(49, 'contabilidade', 'FHKJDH5W9', 10),
+(50, 'RH aprendiz', 'BDJK43', 28),
+(51, 'Marketing', 'HKWDJHKFJSD3637', 2);
 
 -- --------------------------------------------------------
 
@@ -154,11 +182,20 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `cpf`, `dataNascimento`, `situacao_id`, `nivelAcesso_id`, `turma_id`, `endereco_id`, `curso_id`) VALUES
-(1, 'Don pilocarderms souffrant', 'derni@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '80101008970', '2003-11-26', 1, 3, 1, 1, 1),
-(3, 'derni', 'gruda@gmail.com', '4297f44b13955235245b2497399d7a93', '92846458081', '2001-11-09', 1, 3, 1, 1, 6),
+(1, 'Don pilocarderms souffrant', 'derni@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '80101008970', '2003-11-26', 1, 3, 1, 1, 6),
 (5, 'Don Carderms root', 'donroot@gmail.com', '9bfdd62b0c6a8c3732f3826be2b2e044', '44287299001', '2003-11-26', 1, 1, NULL, NULL, NULL),
-(8, 'Alan Renato', 'alanrenatoprof@gmail.com', '21434542', '92836458081', '1995-07-17', 1, 2, 1, 3, 1),
-(9, ' Iraneide Soares da Silva', ' Iraneidedasilva@outlook.com', '435345346', '32435345432', '1993-03-12', 1, 2, NULL, NULL, 3);
+(8, 'Alan Renato', 'alanrenatoprof@gmail.com', '21434542', '92836458081', '1995-07-17', 1, 2, 1, 3, 6),
+(9, ' Iraneide Soares da Silva', ' Iraneidedasilva@outlook.com', '435345346', '32435345432', '1993-03-12', 1, 2, NULL, NULL, NULL),
+(57, 'mariana da silva', 'mariadav@gmail.com', '4bb4280ff186ffa21dfaf7052b52b768', '54213923095', '2001-12-08', 1, 3, 2, 29, 3),
+(86, 'Donerms boy ', 'donerms@gmail.com', '9b13e59273c8a9e5a9b46c1cabcf3255', '85858358243', '2001-12-13', 1, 3, 25, 61, 45),
+(90, 'Caleb Thiago Ferreira', 'caleb_thiago_ferreira@jeffersonbarroso.com.br', '2b1e9d5be9c5a91656c5b5a587de06ac', '13062410655', '1997-11-22', 1, 3, 25, 69, 45),
+(93, 'Rafaela Teresinha Cristiane Gonçalves', 'hghg@gmail.com', '454ff7d4527c0d56006fb6e41d432234', '85858898243', '2022-11-15', 1, 3, 25, 61, 32),
+(95, 'Sérgio Matheus Alexandre Martins', 'sergio-martins86@pontofinalcafe.com.br', '0dd145d84357a1c735085921860957f1', '24367014746', '1995-10-02', 1, 3, 26, 87, 41),
+(97, 'Antonella Gabriela Vanessa Ferreira', 'antonellagabrielaferreira@corpoclinica.com.br', '5e156bc502739ce1440126b39d2abbf8', '87592664224', '1959-03-01', 1, 3, 25, 89, 45),
+(98, 'Patrícia Cláudia Nina Silveira', 'patricia_claudia_silveira@com.br', '95cbbcbc467825b5b650e13854e8dd4c', '40519133170', '1950-01-16', 1, 3, 26, 90, 41),
+(102, 'Rafaela Teresinha Cristiane Gonçalves', 'deeeeeerni@gmail.com', '52c69e3a57331081823331c4e69d3f2e', '07865335807', '2022-10-05', 1, 3, 2, 93, 2),
+(106, 'manu davila', 'davi090@outlook.com', 'cc68e6c7f10425a28251e6f770a745dd', '85158358243', '2002-06-29', 1, 3, 49, 96, 10),
+(107, 'malini caleb', 'calebtrt@outlook.com', 'd367b10d880b793908d522d8f292a6e9', '85828158243', '2001-11-12', 1, 3, 51, 95, 2);
 
 --
 -- Índices para tabelas despejadas
@@ -194,6 +231,7 @@ ALTER TABLE `situacao`
 ALTER TABLE `turma`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `codigo` (`codigo`),
   ADD KEY `curso_id` (`curso_id`);
 
 --
@@ -219,13 +257,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT de tabela `endereco`
 --
 ALTER TABLE `endereco`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- AUTO_INCREMENT de tabela `situacao`
@@ -237,13 +275,13 @@ ALTER TABLE `situacao`
 -- AUTO_INCREMENT de tabela `turma`
 --
 ALTER TABLE `turma`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 
 --
 -- Restrições para tabelas despejadas
